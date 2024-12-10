94bfe12c3cbd5fe7f66c62e7c62e6c1e8d580e37
Cristobal Ríos <cristobalrios2001@gmail.com>
2024-12-10 16:57:55 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,22 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuración de los pipes globales para validación
   app.useGlobalPipes(
     new ValidationPipe({ 
-      transform: true,  // Esta línea es crucial
+      transform: true,  // Transformar automáticamente los datos a los tipos esperados
       transformOptions: {
-        enableImplicitConversion: true
-      }
-    })
+        enableImplicitConversion: true,  // Convertir valores implícitamente
+      },
+    }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Definir el puerto de la aplicación
+  const port = process.env.PORT ?? 3000;
+  console.log(`🚀 Application is running on port: ${port}`);
+
+  // Iniciar la aplicación
+  await app.listen(port);
 }
+
 bootstrap();
